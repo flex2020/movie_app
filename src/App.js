cd017fb2545163b331ad141f16179d800bfcce79
NOH UIBIN <hosan_1@naver.com>
2023-03-26 23:26:16 +0900

    똑똑하게 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,21 +1,63 @@
+import PropTypes from 'prop-types';
 
-function Food({ fav }) {
-  return <h1>I like { fav }.</h1>
+function Food({ name, picture, rating }) {
+  return (
+    <div>
+      <h2>I like { name }.</h2>
+      <h4>{ rating }/5.0</h4>
+      <img src={ picture } alt={ name } Style="width: 600px" />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    id: 1,
+    name: 'Kimchi',
+    image: 'https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png',
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: 'Samgyeopsal',
+    image: 'https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/5atp/image/18MvHleWoHzajN-4Tyruq9d42Vk.jpeg',
+    rating: 4.9,
+  },
+  {
+    id: 3,
+    name: 'Bibimbap',
+    image: 'https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/S9Y/image/76pr0EfQi6R21MMIyzefN9hiqIU.jpg',
+    rating: 3,
+  },
+  {
+    id: 4,
+    name: 'Doncasu',
+    image: 'https://post-phinf.pstatic.net/MjAyMDA0MTZfMjY3/MDAxNTg2OTk5MzYwOTMw.YZk3XJCkJqOrZmSzXTGfnXcfoj5CoLQfY9kEBhBmlyYg.WdEgjt1SmPLlCfi8nmVMB79FymTDi3ApEfQJrGF57Acg.JPEG/1.jpg?type=w1200',
+    rating: 4,
+  },
+  {
+    id: 5,
+    name: 'Kimbap',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Gimbap_%28pixabay%29.jpg',
+    rating: 4,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>
-      Hello
-      </h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chukumi" />
+    {foodILike.map(dish => (
+      <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
+    ))}
     </div>
     
   );
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
 }
 
 export default App;
