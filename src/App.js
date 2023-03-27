407ebbdafbe5f29b8313216f1ea5fae915b54f4d
NOH UIBIN <hosan_1@naver.com>
2023-03-27 20:36:56 +0900

    State 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,63 +1,19 @@
-import PropTypes from 'prop-types';
-
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like { name }.</h2>
-      <h4>{ rating }/5.0</h4>
-      <img src={ picture } alt={ name } Style="width: 600px" />
-    </div>
-  );
-}
-
-const foodILike = [
-  {
-    id: 1,
-    name: 'Kimchi',
-    image: 'https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png',
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: 'Samgyeopsal',
-    image: 'https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/5atp/image/18MvHleWoHzajN-4Tyruq9d42Vk.jpeg',
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: 'Bibimbap',
-    image: 'https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/S9Y/image/76pr0EfQi6R21MMIyzefN9hiqIU.jpg',
-    rating: 3,
-  },
-  {
-    id: 4,
-    name: 'Doncasu',
-    image: 'https://post-phinf.pstatic.net/MjAyMDA0MTZfMjY3/MDAxNTg2OTk5MzYwOTMw.YZk3XJCkJqOrZmSzXTGfnXcfoj5CoLQfY9kEBhBmlyYg.WdEgjt1SmPLlCfi8nmVMB79FymTDi3ApEfQJrGF57Acg.JPEG/1.jpg?type=w1200',
-    rating: 4,
-  },
-  {
-    id: 5,
-    name: 'Kimbap',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Gimbap_%28pixabay%29.jpg',
-    rating: 4,
-  },
-];
+import { useState } from "react";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 6000);
+
   return (
     <div>
-    {foodILike.map(dish => (
-      <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-    ))}
+      { isLoading ? 'Loading...' : 'We are ready.' }
     </div>
-    
-  );
-}
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
+  );
 }
 
 export default App;
