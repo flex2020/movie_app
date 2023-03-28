fa42d5aef0a9f18e5b482d69f26baea4bf48139a
NOH UIBIN <hosan_1@naver.com>
2023-03-28 21:10:27 +0900

    6장 영화 앱 만들기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import Movie from "./Movie";
+import './App.css';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [movies, setMovies] = useState([]);
 
-  setTimeout(() => {
+  const getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
     setIsLoading(false);
-  }, 6000);
-
+    setMovies(movies);
+  }
+  getMovies();
   return (
-    <div>
-      { isLoading ? 'Loading...' : 'We are ready.' }
-    </div>
-
+    <section className="container">
+      { isLoading ? (
+        <div className="loader">
+          <span className="loader__text">Loading...</span>
+        </div>
+      )
+      : (
+        <div>
+          {
+          movies.map((movie) => {
+            return (
+              <Movie 
+                key={movie.id}
+                id={movie.id} 
+                title={movie.title} 
+                year={movie.year} 
+                summary={movie.summary} 
+                poster={movie.medium_cover_image} />
+            );
+            })
+          }
+        </div>
+      )}
+    </section>
   );
 }
 
